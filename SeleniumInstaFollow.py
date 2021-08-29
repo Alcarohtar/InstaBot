1719f54962946ce46d2ab862a3e4dc0f8aa1f1ce
Luigi Rocco <luigirocco86@gmail.com>
2021-08-29 15:31:22 +0200

    Modifica per chiudere automaticamente i popup

--- SeleniumInstaFollow.py
+++ SeleniumInstaFollow.py
@@ -9,16 +9,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-#################################
-## PROGRAMMA BOT PER INSTAGRAM ##
-#################################
-
-#VARIABILI GENERICHE
-#Lista commenti default
+###################################
+### PROGRAMMA BOT PER INSTAGRAM ###
+###################################
+#Variabili Generiche
+selezione_ok=0
+PuoiProcedere=0
+#Inclusione file dei commenti
 in_file_comment = open("comment_InstaFollow", "r")
 ListaCommenti=in_file_comment.readlines()
 in_file_comment.close()
-#ListaCommenti = ["bellissima", "grande scatto", "fantastica", "davvero bella", "top", "mi piace molto"]
+#Inclusione file dei tag
 in_file_tag = open("tag_InstaFollow", "r")
 listaTag = in_file_tag.readlines()
 in_file_tag.close()
@@ -43,41 +44,40 @@ def MiPiace():
     '''Mette un mi piace'''
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".fr66n > button:nth-child(1) > div:nth-child(1) > span:nth-child(1) > svg:nth-child(1)")))
     Like = browser.find_element_by_css_selector(".fr66n > button:nth-child(1) > div:nth-child(1) > span:nth-child(1) > svg:nth-child(1)")
-    #Like.click()
+    Like.click() #eliminare questa riga per debuggare e non inserire mi piace a caso
     sleep(1)
 
 
 def Commenta():
     '''Commenta le foto ognuna con un commento preso a caso da una lista'''
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".Ypffh")))
-    #CommentButton = browser.find_element_by_css_selector("._15y0l > button:nth-child(1) > div:nth-child(1) > svg:nth-child(1) > path:nth-child(1)")
-    #CommentButton.click()
     sleep(2)
-    #commentoVariabile="great"
     TextArea = browser.find_element_by_class_name("Ypffh")
     TextArea.clear()
     TextArea.click()
     TextArea = browser.find_element_by_class_name("Ypffh")
     commentoVariabile = random.choice(ListaCommenti)
     TextArea.send_keys(commentoVariabile)
-    sleep(2)
+    sleep(3)
     PubblicaButton = browser.find_element_by_css_selector("button.sqdOP:nth-child(4)")
-    #PubblicaButton.click()
+    PubblicaButton.click() #eliminare questo quando si vuole debuggare e non aggiungere commenti a caso
     sleep(3)
 
 # Inizializzo webdriver
 browser = webdriver.Firefox()
 browser.get('https://www.instagram.com/')
 sleep(5)
-
 username = input("Inserisci il tuo user: ")
 password = input("Inserisci la tua password: ")
 
-selezione_ok=0
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[4]/div/div/button[1]")))
-Cookie = browser.find_element_by_xpath("/html/body/div[4]/div/div/button[1]")
-Cookie.click()
-sleep(2)
+try:
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[4]/div/div/button[1]")))
+    Cookie = browser.find_element_by_xpath("/html/body/div[4]/div/div/button[1]")
+    Cookie.click()
+except:
+    print("Cookie non presente")
+
+sleep(6)
 usernameInput = browser.find_elements_by_class_name("_2hvTZ")[0]
 passwInput = browser.find_elements_by_class_name("_2hvTZ")[1]
 AccediInput = browser.find_elements_by_class_name("Igw0E")[0]
@@ -86,8 +86,35 @@ usernameInput.send_keys(username)
 passwInput.send_keys(password)
 AccediInput.click()
 print("Chiudi tutti i popup aperti")
+sleep(6)
 
-sleep(10)
+#Questo while serve per chiudere, nel caso fossero presenti, tutti i popup
+while not (PuoiProcedere):
+    try:
+        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.sqdOP:nth-child(1)")))
+        SalvareLeInfo = browser.find_element_by_css_selector("button.sqdOP:nth-child(1)")
+        SalvareLeInfo.click()
+        print("...........")
+    except:
+        print("...........")
+
+    try:
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.aOOlW:nth-child(2)")))
+        AttivaLeNotifiche = browser.find_element_by_css_selector("button.aOOlW:nth-child(2)")
+        AttivaLeNotifiche.click()
+        print("...........")
+    except:
+        print("...........")
+
+    try:
+        Homepage = browser.find_element_by_css_selector(".cGcGK > div:nth-child(2) > div:nth-child(1)").is_selected()
+        PuoiProcedere = 1
+    except:
+        PuoiProcedere = 0
+
+    print("Ok, let's go...")
+sleep(2)
+
 print(""
       "Seleziona una delle seguenti opzioni:\n"
       "1: Like a foto\n"
@@ -108,8 +135,6 @@ while selezione_ok == 0:
         selezione_ok=0
 
 
-#listaTag = input("Inserisci i tag da ricercare (max 3), senza asterisco e con uno spazio tra ogni parola: " ).split()
-print(listaTag)
 numeroTag = len(listaTag)
 numeroFotoDaVisual = int(input("A quante foto per ogni tag vuoi mettere un like o un commento?: "))
 
